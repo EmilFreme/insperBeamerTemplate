ca26f7ac84902587ac5f6dd8b923af682e11df7b
Emil Freme <pedrohepf@insper.edu.br>
2019-05-29 14:43:53 +0000

    Adicionado exemplo e script install generico

--- install.py
+++ install.py
@@ -2,29 +2,36 @@
 import os
 
 def install():
-    if "linux" in  os.sys.platform:
-        texlivePath = "/usr/share/texlive/texmf-dist/tex/latex/beamer"
-        print(os.path.exists(texlivePath))
-        if(os.path.isdir(texlivePath)):
+    if not "win32" in  os.sys.platform:
+        paths = os.popen("find /urs -name \
+beamerthemedefault.sty").readlines()
+        beamerPath = paths[0].replace("beamerthemedefault.sty\\n", "");
+
+        print(os.path.exists(beamerPath))
+        if(os.path.isdir(beamerPath)):
             print("Texlive beamer path found...")
+            print(beamerPath)
             print("Copying theme file into beamer folder...")
             os.system("cp beamerthemeinsper.sty \
-{}/beamerthemeinsper.sty".format(texlivePath))
+{}/beamerthemeinsper.sty".format(beamerPath))
             print("Copying themeinner file into beamer folder...")
             os.system("cp beamerinnerthemeinsper.sty \
-{}/beamerinnerthemeinsper.sty".format(texlivePath))
+{}/beamerinnerthemeinsper.sty".format(beamerPath))
             print("Copying themeouter file into beamer folder...")
             os.system("cp beamerouterthemeinsper.sty \
-{}/beamerouterthemeinsper.sty".format(texlivePath))
+{}/beamerouterthemeinsper.sty".format(beamerPath))
             print("Copying themecolor file into beamer folder...")
             os.system("cp beamercolorthemeinsper.sty \
-{}/beamercolorthemeinsper.sty".format(texlivePath))
+{}/beamercolorthemeinsper.sty".format(beamerPath))
             print("Copying themefont file into beamer folder...")
             os.system("cp beamerfontthemeinsper.sty \
-{}/beamerfontthemeinsper.sty".format(texlivePath))
+{}/beamerfontthemeinsper.sty".format(beamerPath))
             print("Copying images folter into beamer folder...")
             os.system("cp -a img/. \
-{}/img/".format(texlivePath))
+{}/img/".format(beamerPath))
+        else:
+            print("Script para windows em construção")
+            print("Não instalado")
 
 if __name__ == "__main__":
     install()
